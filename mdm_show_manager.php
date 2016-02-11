2f0992d76fb979c07a4210dfddbb0eb65f140e32
Bob Moore <bobjutsu@gmail.com>
2016-02-11 09:09:18 -0600

    Version update

--- mdm_show_manager.php
+++ mdm_show_manager.php
@@ -11,7 +11,7 @@
  * Plugin Name: MDM Show Manager
  * Plugin URI:  http://midwestfamilymarketing.com
  * Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:     1.0.1
+ * Version:     1.0.2
  * Author:      Mid-West Family Marketing
  * Author URI:  http://midwestfamilymarketing.com
  * License:     GPL-2.0+
