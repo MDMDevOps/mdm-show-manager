599f24ca9cc148d7e6f7f41e9bea40985522903a
Bob Moore <bobjutsu@gmail.com>
2016-03-11 12:03:02 -0600

    Bug Fix: Calendar date object skipping times

--- admin/partials/admin_display_list.php
+++ admin/partials/admin_display_list.php
@@ -7,29 +7,23 @@
  * @package mdm_show_manager
  */
 ?>
-<?php
-
-?>
-<?php add_thickbox(); ?>
 
 <?php // Feature in development : include plugin_dir_path( __FILE__ ) . 'display_calendar_form.php'; ?>
 
 <div id="mdmsm_calendar">
     <div class="mdmsm_calendar_wrapper">
-        <!-- reset time -->
-        <?php $this->reset_time(); ?>
         <!-- Time Label Column -->
         <ol class="calendar-time">
             <!-- Set left column 'header' -->
             <li class="header"><?php _e( 'Day', $this->plugin_name ); ?></li>
             <!-- Set left column time labels -->
             <?php for( $i = 0; $i < 48; $i++  ) : ?>
-                <li class="timelabel" data-time="<?php echo $this->time->format( 'h:i:s' ); ?>"><span class="label"><?php echo $this->time->format( 'h:i A' ); ?></span></li>
-                <?php $this->time->add( new DateInterval('PT30M') ); ?>
+                <li class="timelabel" data-time="<?php echo $time->format( 'h:i:s' ); ?>"><span class="label"><?php echo $time->format( 'h:i A' ); ?></span></li>
+                <?php $time->add( new DateInterval('PT30M') ); ?>
             <?php endfor; ?>
         </ol>
         <!-- reset time -->
-        <?php $this->reset_time(); ?>
+        <?php $time->setTime( 0, 0, 0 ); ?>
         <!-- Set calendar days -->
         <div class="mdmsm_calendar_slots">
         <?php for( $day = 1; $day <= 7; $day++ ) : ?>
@@ -38,13 +32,13 @@
                 <li class="header"><?php echo $this->get_day_name( $day ) ?></li>
                 <!-- Set individuatl rows -->
                 <?php for( $slot = 0; $slot < 48; $slot++  ) : ?>
-                    <?php echo $this->get_calendar_row( $slot, $day, $this->time->format( 'H:i:s' ) ); ?>
+                    <?php echo $this->get_calendar_row( $slot, $day, $time->format( 'H:i:s' ) ); ?>
                     <!--Increment time object -->
-                    <?php $this->time->add( new DateInterval('PT30M') ); ?>
+                    <?php $time->add( new DateInterval('PT30M') ); ?>
                 <?php endfor; ?>
             </ol>
             <!-- reset time -->
-            <?php $this->reset_time(); ?>
+            <?php $time->setTime( 0, 0, 0 ); ?>
         <?php endfor; ?>
         </div>
     </div>
